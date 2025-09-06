6389df530b4db725a080a039701e05d3b68672c1
Em <anotherdesignpractice@gmail.com>
2025-09-06 16:44:04 -0700

    v1

--- library/script.js
+++ library/script.js
@@ -1,57 +1,50 @@
 function calculate() {
+        
+    const { format } = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        maximumFractionDigits: 0,
+    });
 
-//  variables
+    let income = takeHomePay.value;
+    let needs = income*0.5;
+    let wants = income*0.3;
+    let savings = income*0.2;
 
-    let salary = inputSalary.value;
-    let needs = generateString(salary,0.5);
-    let wants = generateString(salary,0.3);
-    let savings = generateString(salary,0.2);
-    let housing25 = generateString(salary,0.25);
-    let housing30 = generateString(salary,0.3);
-    let emergency3 = generateString(needs,3);
-    let emergency6 = generateString(needs,6);
-    let emergency9 = generateString(needs,9);
+    budgetNeeds.innerHTML = `Needs ${format(needs)}`;
+    budgetWants.innerHTML = `Wants ${format(wants)}`;
+    budgetSavings.innerHTML = `Savings ${format(savings)}`;
 
-//  display string
+    let income25= income*0.25;
+    let income30= income*0.30;
 
-    displayString('wants', wants);
-    displayString('needs', needs);
-    displayString('savings', savings);
-    displayString('housing25', housing25);
-    displayString('housing30', housing30);
-    displayString('emergency3', emergency3);
-    displayString('emergency6', emergency6);
-    displayString('emergency9', emergency9);
-    
+    housingLow.innerHTML = `25% is ${format(income25)}`;
+    housingLowPoint1.innerHTML = `${format(needs-income25)} left for needs`;
+    housingLowPoint2.innerHTML = `${format(wants)} left for wants`;
+    housingLowPoint3.innerHTML = `${format(savings)} left for savings`;
+
+    housingHigh.innerHTML = `30% is ${format(income30)}`;
+    housingHighPoint1.innerHTML = `${format(needs-income30)} left for needs`;
+    housingHighPoint2.innerHTML = `${format(wants)} left for wants`;
+    housingHighPoint3.innerHTML = `${format(savings)} left for savings`;
+
+    let savingsNeedsUnit = income*0.5;
+
+    savings3.innerHTML = `${format(savingsNeedsUnit*3)} lasts 3 months of needs`;
+    savings3Time1.innerHTML = `This takes ~${savingsNeedsUnit*3/(income*0.2)} months to save`;
+    savings3Time2.innerHTML = `Or ~${savingsNeedsUnit*3/(income*0.5)} months if you only spend on needs`;
+    savings3Time3.innerHTML = `Or ~${savingsNeedsUnit*3/income} months if you spend nothing`;
+
+    savings6.innerHTML = `${format(savingsNeedsUnit*6)} lasts 6 months of needs`;
+    savings6Time1.innerHTML = `This takes ~${savingsNeedsUnit*6/(income*0.2)} months to save`;
+    savings6Time2.innerHTML = `Or ~${savingsNeedsUnit*6/(income*0.5)} months if you only spend on needs`;
+    savings6Time3.innerHTML = `Or ~${savingsNeedsUnit*6/income} months if you spend nothing`;
+
+    savings9.innerHTML = `${format(savingsNeedsUnit*9)} lasts 9 months of needs`;
+    savings9Time1.innerHTML = `This takes ~${savingsNeedsUnit*9/(income*0.2)} months to save`;
+    savings9Time2.innerHTML = `Or ~${savingsNeedsUnit*9/(income*0.5)} months if you only spend on needs`;
+    savings9Time3.innerHTML = `Or ~${savingsNeedsUnit*9/income} months if you spend nothing`;
 }
-
-/*  calculations & formatting
-
-    1. Bring in string     4,000
-    2. Remove comma        4000
-    3. Do math             4000 * 0.3 = 1333.33
-    4. Make whole number   1333
-    5. Add comma back in   1,333
-*/
-    function generateString(base, factor) {
-
-        return Math.round(base.replace(/,/g, '')*factor).toLocaleString('en-US');
-
-    }
-
-//  reusable strings
-
-function displayString(className, text) {
-
-    var elms = document.querySelectorAll('.' + className), i;
-
-    for (i = 0; i < elms.length; ++i) {
-        elms[i].textContent = text;
-    }      
-
-}
-
-//  accordion
 
 var acc = document.getElementsByClassName("accordion");
 var i;
